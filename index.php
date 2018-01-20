08650ac68ac594690ad3ec566e3568738382845f
onval <giorgio.valbonesi@gmail.com>
2018-01-20 11:35:49 +0100

    Create foundation of main page

--- index.php
+++ index.php
@@ -2,12 +2,41 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css" type="text/css" >
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css" >
+    <title>Crowdsourcing</title>
 </head>
 <body>
-    <?php
-        echo "<h2>Crowdsourcing database project!</h2>"
-    ?>
+    <header>
+        <!-- <img src=img/black_keyboard.jpg alt="black-keyboard" width="100%" height="400px"> -->
+        <h1> CROWDSAUCING </h1>
+        <p>Welcome to the crowdsourcing university project for the database course</p>
+        <p>Universitá Statale di Milano.</p>
+    </header>
+
+    <section class="join_container" >
+        <section id="worker" class="single_join_block"> 
+            <button> Join as a worker </button>
+            <a href="#" class="needtologin"> Already have an account? Sign in </a>
+            <p class="description">
+                Description for what a worker is and what it does
+                The worker has the job of working a lot balbalbal amazing
+                Description for what a worker is and what it does
+                The worker has the job of working a lot balbalbal amazing
+            </p>
+        </section><section id="requester" class="single_join_block">
+            <button> Join as a requester </button>
+
+            <a href="#" class="needtologin"> Already have an account? Sign in </a>
+            <p class="description">
+                Description for what a requester is and what it does
+                The requester has the job of creating stuff
+                Description for what a requester is and what it does
+                The requester has the job of creating stuff
+            </p>
+        </section>
+    </section>
+
+    <footer> Made by Giorgio Valbonesi </footer>
+
 </body>
 </html>
