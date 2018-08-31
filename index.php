032bd57d25f576b949b33312877f5433f46981d3
onval <giorgio.valbonesi@gmail.com>
2018-08-31 18:16:19 +0200

    Unifying registration-form after a database refactor

--- index.php
+++ index.php
@@ -13,31 +13,7 @@
     </header>
 
     <section class="join_container" >
-        <section id="worker" class="single_join_block"> 
-            <a href="#" class='button'> Join as a worker </a>
-            <a href="#" class="needtologin-w"> Already have an account? Sign in </a>
-            <div class="description">
-                <p>
-                Workers are users who perform tasks.
-                </p>
-                <p>They have a profile that includes certain keywords which represent
-                    the worker's affiliations and knowledge on specific topics.
-                 </p>
-            </div>
-        </section><section id="requester" class="single_join_block">
-            <a class='button' href='requester-form.php'> Join as a requester </a>
-
-            <a href="requester-login.php" class="needtologin-r"> Already have an account? Sign in </a>
-            <div class="description">
-                <p>The requester is responsible of creating and promoting work campaigns.</p>
-                <p> After campaign creation, the requester defines:</p>
-                <ul>
-                    <li>the tasks to be done</li>
-                    <li>the n. of workers involved in each task</li>
-                    <li>the threshold to consider valid a task result</li>
-                </ul>
-            </div>
-        </section>
+        <a href="registration-form.php" class='button'> Register now </a>
     </section>
 
     <footer> Made by Giorgio Valbonesi </footer>
